da4b77159f0572b03f03ae54c020f063a5567de6
Raciel <marcofenix1993@gmail.com>
2022-10-19 22:17:40 +0200

    Add models to administration panel

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
-# Register your models here.
+from api.models import Drone, Medication
+
+
+@admin.register(Drone)
+class DroneAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(Medication)
+class MedicationAdmin(ModelAdmin):
+    pass
